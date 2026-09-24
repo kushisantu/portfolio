93c1f36e7046aa08427b022efd38336fcc4cb012
Kushi Khandoji <kushisantutennis@gmail.com>
2026-09-23 20:49:18 -0400

    base additions

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Kushi Santosh Khandoji</h1>
+      <p>M.S. Computer Science, George Washington University</p>
+    </main>
+  )
+}
+
+export default App
